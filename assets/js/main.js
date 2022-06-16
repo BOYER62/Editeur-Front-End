58dd8c415906c8749583278d5a6d1280810cf931
BOYER62 <boyer.olivier2@free.fr>
2022-06-16 13:57:02 +0200

    html et css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,23 @@
-const ValHtml = document.getElementById('IdHtml');
-const ValCss = document.getElementById('IdCss');
-const ValJs = document.getElementById('IdJs');
-const ValResult = document.getElementById('IdResult');
+const valHtml = document.getElementById('idHtml');
+const valCss = document.getElementById('idCss') ;
+const valJs = document.getElementById('idJs');
+const valResult = document.getElementById('idResult');
+const valTextHtmlCssJs = document.getElementById('idTextHtmlCssJs');
+const valTextCss = document.getElementById('idTextCss');
 
-document.getElementsByClassName('ClassHtml')[0].focus();
-document.getElementsByClassName('ClassCss')[0].focus();
-ValHtml.addEventListener('keyup', function() {
-// function render(){
-    html = document.getElementsByClassName('ClassHtml')[0].value;
-    document.getElementsByClassName('ClassResult')[0].innerHTML = html;
+
+valHtml.addEventListener('keyup', function(){
+    valResult.innerHTML = valHtml.value;
 });
 
-ValCss.addEventListener('keyup', function() {
-        html = document.getElementsByClassName('ClassCss')[0].value;
-        document.getElementsByClassName('ClassResult')[0].innerHTML = html;
-    });
+valCss.addEventListener('keyup', function(){
+    valTextHtmlCssJs.innerHTML += '<style>' + valCss.value +'</style>';
+});
 
-//document.getElementsByClassName('ClassResult')[0].innerHTML = html + "<script>" + htmlCss + "</script>"
+valJs.addEventListener('keyup', function(){
+    valTextHtmlCssJs.innerHTML += '<script>' + valJs.value +'</script>';
+    console.log(valTextHtmlCssJs);
+});
+
+// const textAll= `${valHtml.value} <style>${valCss.value}</style> <script ${valJs.value} </script>`;
+// console.log(textAll);
