d256481369d727f2d0a9b2eaa669a17cb1f04142
BOYER62 <boyer.olivier2@free.fr>
2022-06-16 21:24:14 +0200

    caractere max

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,22 +2,31 @@ const valHtml = document.getElementById('idHtml');
 const valCss = document.getElementById('idCss') ;
 const valJs = document.getElementById('idJs');
 const valResult = document.getElementById('idResult');
-const valTextHtmlCssJs = document.getElementById('idTextHtmlCssJs');
+const valTextJs = document.getElementById('idTextJs');
 const valTextCss = document.getElementById('idTextCss');
-
+const valCarractereMax = document.getElementById('idCarractereMax');
+const valValeurMax = document.getElementById('idValeurMax');
+let nombreCarractere = 0;
+let carractereMax = 250;
+let valBalise = '';
+valCarractereMax.innerHTML = nombreCarractere + '/' + carractereMax;
 
 valHtml.addEventListener('keyup', function(){
+    if(nombreCarractere <= carractereMax){
     valResult.innerHTML = valHtml.value;
-});
+    valBalise = valResult.textContent;
+    nombreCarractere = valBalise.length;
+    valCarractereMax.innerHTML = nombreCarractere + '/' + carractereMax;
+}});
 
 valCss.addEventListener('keyup', function(){
-    valTextHtmlCssJs.innerHTML += '<style>' + valCss.value +'</style>';
+    valTextCss.innerHTML = '<style>' + valCss.value +'</style>';
 });
 
 valJs.addEventListener('keyup', function(){
-    valTextHtmlCssJs.innerHTML += '<script>' + valJs.value +'</script>';
-    console.log(valTextHtmlCssJs);
+    valTextJs.innerHTML = '<script>' + valJs.value +'</script>';
 });
 
-// const textAll= `${valHtml.value} <style>${valCss.value}</style> <script ${valJs.value} </script>`;
-// console.log(textAll);
+valValeurMax.addEventListener('mousemove', function(e){
+    valValeurMax.style.left = e.x / window.innerWidth * 100 + '%';
+});
