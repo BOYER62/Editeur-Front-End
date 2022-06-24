cb0b638301e5c4cdcffe07d89957ecd1049b3f96
BOYER62 <boyer.olivier2@free.fr>
2022-06-24 22:38:38 +0200

    ajout Messenger FB

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ const valTextCss = document.getElementById('idTextCss');
 const valCarractereMax = document.getElementById('idCarractereMax');
 const valValeurMax = document.getElementById('idValeurMax');
 const valBoutonProgresseBar = document.getElementById('idBoutonProgresseBar');
+const valLogo = document.getElementById('idLogo');
 
 let nombreCarractere = 0;
 let carractereMax = 250;
@@ -15,11 +16,15 @@ let valBalise = '';
 valCarractereMax.innerHTML = nombreCarractere + '/' + carractereMax;
 
 valHtml.addEventListener('keyup', function(){
+    logo('logoHtml');
     if(nombreCarractere < carractereMax){
     valResult.innerHTML = valHtml.value;
     valBalise = valResult.textContent;
     nombreCarractere = valBalise.length;
     const valWidth = (nombreCarractere / carractereMax) * 100 ;
+    if(valWidth <= 50){
+        valBoutonProgresseBar.style.backgroundColor = 'green';
+    }
     if(valWidth > 50 && valWidth < 80){
         valBoutonProgresseBar.style.backgroundColor = 'orange';
     }
@@ -30,10 +35,12 @@ valHtml.addEventListener('keyup', function(){
     valCarractereMax.innerHTML = nombreCarractere + '/' + carractereMax;
 }});
 valCss.addEventListener('keyup', function(){
+    logo('logoCss');
     valTextCss.innerHTML = '<style>' + valCss.value +'</style>';
 });
 
 valJs.addEventListener('keyup', function(){
+    logo('logoJs');
     valTextJs.innerHTML = '<script defer>' + valJs.value +'</script>';
 });
 
@@ -44,3 +51,31 @@ valValeurMax.addEventListener('mousemove', function(e){
     valCarractereMax.innerHTML = nombreCarractere + '/' + carractereMax;
 });
 
+function logo(image){
+    switch(image){
+        case 'logoHtml':
+            valLogo.classList.remove('logoCss');
+            valLogo.classList.remove('logoJs');
+            valLogo.classList.add('logoHtml');
+            valLogo.classList.add('rotate-vert-center');
+            break;
+        case 'logoCss':
+            valLogo.classList.remove('logoHtml');
+            valLogo.classList.remove('logoJs');
+            valLogo.classList.add('logoCss');
+            valLogo.classList.add('rotate-vert-center');
+            break;
+        case 'logoJs':
+            valLogo.classList.remove('logoHtml');
+            valLogo.classList.remove('logoCss');
+            valLogo.classList.add('logoJs');
+            valLogo.classList.add('rotate-vert-center');
+            break;
+        default :
+            valLogo.classList.remove('logoHtml');
+            valLogo.classList.remove('logoCss');
+            valLogo.classList.remove('logoJs');
+            valLogo.classList.remove('rotate-vert-center');
+            break;
+    }
+}
